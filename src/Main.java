7330e507fb79940d112244362e95aadcc9efa62c
NieT29 <tienlevan3010@gmail.com>
2023-09-30 14:33:01 +0700

    update1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -9,17 +13,21 @@ public class Main {
 
         System.out.println("Mời bạn nhập ngày tháng năm sinh (yyyy/MM/dd): ");
         String dateOfBirth = scanner.nextLine();
+        LocalDate lcd = LocalDate.parse(dateOfBirth, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
 
         System.out.println("Mời bạn nhập thời gian nhập hàng (yyyy/MM/dd HH:mm:ss): ");
-        String timeIn = scanner.nextLine();
+        String time1 = scanner.nextLine();
+        LocalDateTime lcdt = LocalDateTime.parse(time1, DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
 
         System.out.println("Mời bạn nhập thời gian (HH:mm:ss)");
-        String time = scanner.nextLine();
+        String time2 = scanner.nextLine();
+        LocalTime lct = LocalTime.parse(time2, DateTimeFormatter.ofPattern("HH:mm:ss"));
 
 
         System.out.println("Tên hàng nhập kho là: "+name);
-        System.out.println("Ngày tháng năm sinh của bạn là: "+dateOfBirth);
-        System.out.println("Thời gian bạn nhập hàng là: "+timeIn);
-        System.out.println("Thời gian của bạn: "+time);
+        System.out.println("Ngày tháng năm sinh của bạn là: "+lcd);
+        System.out.println("Thời gian bạn nhập hàng là: "+lcdt);
+        System.out.println("Thời gian của bạn: "+lct);
+
     }
 }
